a789f9e7264e27650f4fc53d21451362899e4913
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2022-09-26 16:41:01 +0200

    HIPFORGE-421 releasing feeds 7.0.0: delete /docs

--- docs/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.bloomreach.forge.feed.resource","l":"DocumentTypeHelperTest"},{"p":"org.bloomreach.forge.feed","l":"RssAtomGenerationTest"},{"p":"org.bloomreach.forge.feed","l":"RssAtomGenerationTest.TestBean"}]
